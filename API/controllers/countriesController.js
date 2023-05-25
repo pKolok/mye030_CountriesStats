912dd6161ce62193070177d1d4a352f76996928f
panousias <panousias@hotmail.com>
2023-05-25 22:20:12 +0300

    Various: small changes

--- API/controllers/countriesController.js
+++ API/controllers/countriesController.js
@@ -7,18 +7,6 @@ exports.getAllCountries = (req, res) => {
 
         if (err) throw err;
 
-        // Introduce delay. For testing purposes
-        // setTimeout(function() {
-        //     res.status(200).json({
-        //         status: 'success',
-        //         results: rows.length,
-        //         data: {
-        //             countries: JSON.parse(JSON.stringify(rows))
-        //         }
-        //     });
-        //     console.log('This printed after about 5 second');
-        //   }, 5000);
-
         res.status(200).json({
             status: 'success',
             results: rows.length,
